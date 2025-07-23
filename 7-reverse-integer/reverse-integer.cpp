a5ebb0452f06b0b8047176df097fa1b262912cbf
gauravGithub63 <gauravkumar20082003gk@gmail.com>
2025-07-23 20:46:29 +0530

    Time: 2 ms (39.19%) | Memory: 8.6 MB (20.75%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -41,8 +41,8 @@ public:
             int rem = x % 10;
 
             
-            if (ans > INT_MAX / 10 || (ans == INT_MAX / 10 && rem > 7)) return 0;
-            if (ans < INT_MIN / 10 || (ans == INT_MIN / 10 && rem < -8)) return 0;
+            if (ans > INT_MAX / 10 ) return 0;
+            if (ans < INT_MIN / 10 ) return 0;
 
             ans = ans * 10 + rem;
             x = x / 10;
