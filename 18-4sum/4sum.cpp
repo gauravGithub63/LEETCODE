d3bd748ccf076c54594bf1744cf2081fd2a41624
gauravGithub63 <gauravkumar20082003gk@gmail.com>
2025-07-26 23:42:48 +0530

    Time: 23 ms (57.11%) | Memory: 17.4 MB (84.79%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -24,17 +24,17 @@ public:
                     if (sum == target) {
                         ans.push_back({nums[i], nums[j], nums[left], nums[right]});
                         
-                        while (left < right && nums[left] == nums[left + 1]) ++left;
-                        while (left < right && nums[right] == nums[right - 1]) --right;
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
 
-                        ++left;
-                        --right;
+                        left++;
+                        right--;
                     }
                     else if (sum < target) {
-                        ++left;
+                        left++;
                     }
                     else {
-                        --right;
+                        right--;
                     }
                 }
             }
